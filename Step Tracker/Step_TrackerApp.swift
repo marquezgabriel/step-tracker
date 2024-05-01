c82dd7b9d95d0e3445474e38c46582682ddd0bd1
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2024-04-30 20:03:36 -0400

    Initial Commit

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Gabriel Marquez on 2024-04-30.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
